4687874095593c5f09f1d5b42b4aa8117fc301f7
Vietha22 <halongviet22@gmail.com>
2023-10-05 17:59:15 +0700

    nop bt 31

---
+++ Day_31/js/ex01.js
@@ -0,0 +1,47 @@
+const counter = document.querySelector(".counter");
+const btn = document.querySelector(".btn");
+
+const requestAnimationFrame =
+  window.requestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.msRequestAnimationFrame;
+
+const cancelAnimationFrame =
+  window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+
+let requestId;
+
+const initialTime = 2;
+let timePerCb = initialTime * 60;
+let isDisabled = true;
+
+const playCountdown = () => {
+  timePerCb--;
+  let timeLeft = timePerCb / 60;
+  if (timeLeft > 0) {
+    counter.textContent = timeLeft.toFixed();
+    requestId = requestAnimationFrame(playCountdown);
+  } else {
+    isDisabled = false;
+    btn.removeAttribute("disabled");
+  }
+};
+
+btn.addEventListener("click", () => {
+  if (!isDisabled) {
+    window.location.href = "https://fullstack.edu.vn";
+  }
+});
+
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    cancelAnimationFrame(requestId);
+  } else {
+    requestId = requestAnimationFrame(playCountdown);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  requestId = requestAnimationFrame(playCountdown);
+});
