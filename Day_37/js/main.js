e42eb4a06d7caa9f77c455f257b7cb02d98f20a6
Vietha22 <halongviet22@gmail.com>
2023-10-27 21:12:14 +0700

    update

--- Day_37/js/main.js
+++ Day_37/js/main.js
@@ -80,7 +80,7 @@ const app = {
 
     posts.forEach((post) => {
       const postEl = document.createElement("div");
-      postEl.classList.add("post");
+      postEl.classList.add("post", "w-50");
 
       postEl.innerHTML = `
       <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#">
