554dda89eb2c99bd1a452baf3996f305aec75eff
Vietha22 <halongviet22@gmail.com>
2023-11-11 03:05:09 +0700

    fix 40

--- Day_40/singlepage/src/Error.js
+++ Day_40/singlepage/src/Error.js
@@ -1,9 +1,9 @@
-import image from "./Assets/images/404-not-found.jpg";
+// import image from "./Assets/images/404-not-found.jpg";
 import "./Assets/Errors.scss";
 export const Error = () => {
   return `
     <div class="error-page">
-        <img src="${image}" />
+        <img src="./Assets/images/404-not-found.jpg" />
     </div>
     `;
 };
