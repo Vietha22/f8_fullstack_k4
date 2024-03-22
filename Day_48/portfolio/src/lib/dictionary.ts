ade9c5c88619501b494017e74d171e98252bb10f
Vietha22 <halongviet22@gmail.com>
2024-03-22 22:20:18 +0700

    update lang default english

--- Day_48/portfolio/src/lib/dictionary.ts
+++ Day_48/portfolio/src/lib/dictionary.ts
@@ -2,8 +2,8 @@ import "server-only";
 import type { Locale } from "@/i18n.config";
 
 const dictionaries = {
-  vi: () => import("@/dictionaries/vi.json").then((module) => module.default),
   en: () => import("@/dictionaries/en.json").then((module) => module.default),
+  vi: () => import("@/dictionaries/vi.json").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]();
